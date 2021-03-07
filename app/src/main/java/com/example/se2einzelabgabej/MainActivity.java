42cac1ad179c0e49e2111f4ae452d024f01b997c
NicoOtt1998 <nico.ott1998@gmail.com>
2021-03-07 11:42:16 +0100

    2.2 first steps

--- app/src/main/java/com/example/se2einzelabgabej/MainActivity.java
+++ app/src/main/java/com/example/se2einzelabgabej/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.se2einzelabgabej;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btncalc = findViewById(R.id.button2);
+        btncalc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText inputField=findViewById(R.id.editTextNumber);
+                String mk= inputField.getText().toString();
+                mk=addASCII(mk);
+            }
+
+            public String addASCII (String mk){
+                String mkString= String.valueOf(mk);
+                String stringNEw="Nice";
+                return stringNEw;
+
+            }
+        });
     }
 }
