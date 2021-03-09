fd9e5abd0fd576152ef7c3c8b27490142bb387a2
NicoOtt1998 <nico.ott1998@gmail.com>
2021-03-09 15:45:17 +0100

    outsouced 2.2, to get cleaner main

--- app/src/main/java/com/example/se2einzelabgabej/Calculator.java
+++ app/src/main/java/com/example/se2einzelabgabej/Calculator.java
@@ -1,5 +1,26 @@
 package com.example.se2einzelabgabej;
 
-public class MakeIntToAscii {
-    
+public class Calculator {
+
+    //a,b,c,d,e,f,g,h,i,j
+    //1,2,3,4,5,6,7,8,9,0
+    public String makeNewString(String s){
+        String stringNEw="";
+        for(int i=0;  i<s.length();i++){
+            char c = s.charAt(i);
+            if(i%2 != 0){
+                int ascii= c;
+                if(c==48){
+                    ascii+=58;
+                }
+                else{
+                    ascii+=48;
+                }
+                c= (char) ascii;
+            }
+            stringNEw+=c;
+        }
+        return stringNEw;
+    }
+
 }
