999765c9b07a2a5634fed5881b24a8212ac321e7
NicoOtt1998 <nico.ott1998@gmail.com>
2021-03-10 16:24:59 +0100

    Refectort Calculator

--- app/src/main/java/com/example/se2einzelabgabej/Calculator.java
+++ app/src/main/java/com/example/se2einzelabgabej/Calculator.java
@@ -4,21 +4,13 @@ public class Calculator {
 
     //a,b,c,d,e,f,g,h,i,j
     //1,2,3,4,5,6,7,8,9,0
-    public String makeNewString(String s){
-        String stringNEw="";
-        for(int i=0;  i<s.length();i++){
-            char c = s.charAt(i);
-            if(i%2 != 0){
-                if(c==48){
-                    c+=58;
-                }
-                else{
-                    c+=48;
-                }
-            }
-            stringNEw+=c;
+    public String makeNewString(String s) {
+        char[] cArray = s.toCharArray();
+        for (int i = 1; i < s.length(); i+=2) {
+            cArray[i] += cArray[i] == 48 ? 58 : 48;
         }
-        return stringNEw;
+        return new String(cArray);
     }
+
 
 }
